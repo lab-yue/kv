59721f4be86a05610298240dd0cc2452f1188b57
rainy-me <github@rainy.me>
2019-08-25 05:33:59 +0900

    create data dir and file if not exist

--- src/store.rs
+++ src/store.rs
@@ -1,10 +1,11 @@
+extern crate dirs;
 extern crate serde;
 extern crate serde_json;
-extern crate dirs;
 
 use serde::{Deserialize, Serialize};
 use std::fmt;
-use std::fs::File;
+use std::fs;
+use std::io::prelude::*;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Pair {
@@ -24,15 +25,29 @@ pub struct Store {
 }
 
 pub fn read() -> Vec<Pair> {
-    match dirs::home_dir() {
-        Some(mut home_path) => {
-            
-            home_path.push(".data/kv-data.json");
+    match dirs::data_local_dir() {
+        Some(mut data_path) => {
+            data_path.push("kv");
+
+            if !data_path.exists() {
+                #[cfg(debug_assertions)]
+                dbg!(&data_path);
+
+                fs::create_dir(&data_path).expect("failed to create data dir");
+            }
+
+            data_path.push("data.json");
+
+            if !data_path.exists() {
+                let mut file = fs::File::create(&data_path).expect("Failed to create file");
+                file.write_all(b"{\"data\":[]}")
+                    .expect("Failed to create file");
+            }
 
             #[cfg(debug_assertions)]
-            println!("file path, probably: {}", home_path.display());
+            println!("file path, probably: {}", data_path.display());
 
-            let json_file = File::open(home_path).expect("file not found");
+            let json_file = fs::File::open(&data_path).expect("file not found");
             let store: Store =
                 serde_json::from_reader(json_file).expect("error while reading json");
 
